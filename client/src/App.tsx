fe1bdc827f15d8672f279ecee51ecb13c4655b94
Mykyta Nazarov <nnazarov55@gmail.com>
2024-09-30 14:43:14 +0300

    persist state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
 import Chat from './components/Chat/Chat';
-import ColorThemePicker from './components/ColorThemePicker/ColorThemePicker';
+// import ColorThemePicker from './components/ColorThemePicker/ColorThemePicker';
 import { useThemeStore } from './store/theme';
 
 function App() {
-  const themes = useThemeStore((state) => state.themes);
+  // const themes = useThemeStore((state) => state.themes);
   const theme = useThemeStore((state) => state.theme);
 
   return (
     <main
       className={`px-12 py-8 theme-${theme} bg-primary h-screen overflow-hidden`}
     >
-      <ColorThemePicker themes={themes} theme={theme} />
+      {/* <ColorThemePicker themes={themes} theme={theme} /> */}
       <Chat />
     </main>
   );
--- client/src/store/user.ts
+++ client/src/store/user.ts
@@ -1,11 +1,23 @@
 import { create } from 'zustand';
 import { UserType } from '../types/types';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
 
 interface UserState {
   user: UserType | null;
   setUser: (user: UserType) => void;
 }
-export const useUserStore = create<UserState>()((set) => ({
-  user: null,
-  setUser: (user) => set(() => ({ user: user })),
-}));
+export const useUserStore = create<UserState>()(
+  immer(
+    persist(
+      (set) => ({
+        user: null,
+        setUser: (user) => set(() => ({ user: user })),
+      }),
+      {
+        name: 'user',
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
+  ),
+);
